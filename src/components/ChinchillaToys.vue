e971bd7560f14878d69ab59c47ccfea63f13d342
Christa <cweav3r@gmail.com>
2019-01-12 16:03:17 -0500

    adding new pages

---
+++ src/components/ChinchillaToys.vue
@@ -0,0 +1,35 @@
+<template>
+	<section>
+		<h1>Chinchilla Toys</h1>
+		<p>Chinchillas have teeth that grow continuously over their whole life span. Gnawing and chewing on things keep their teeth trimmed and in good condition. This is a natural and necessary activity for pet chinchillas. Having access to plenty of toys ensures optimal teeth health for your chinchilla – not to mention it also satisfies their curiosity and keeps them entertained.</p>
+
+		<h2>Chinchilla Chew Toys</h2>
+		<p>Wood toys such as tree branches, twigs, or manufactured wood pieces are safe for chinchillas. It is important the wood is untreated and pesticide free.  You can purchase chinchilla chew toys in the rodent sections of pet stores, or rope and wood toys found in the bird section. Some chinchillas will chew on pumice stones, mineral blocks, and salt licks. Salt licks also aid in healthy digestion.</p>
+
+		<h3>Safe &amp; Hazardous Woods</h3>
+		<strong>Safe Woods:</strong>
+			<ul>
+				<li>Wood from fruit baring trees (apple, grape, pear, etc.)</li>
+				<li>Aspen</li>
+				<li>Cottonwood </li>
+				<li>Crabapple </li>
+				<li>Dogwood </li>
+				<li>Poplar</li>
+				<li>Willow</li>
+			</ul>
+			
+		<strong>Hazardous Woods:</strong>
+			<ul>
+				<li>Wood from fruit trees with seeds/pits (cherry, peaches, etc.)</li>
+				<li>Cedar</li>
+				<li>Chesnut </li>
+				<li>Citrus fruit trees (lemon, orange, grapefruit, etc.) </li>
+				<li>Evergreen </li>
+				<li>Fir</li>
+				<li>Redwood</li>
+			</ul>
+		
+		<h2>Other Safe Toys</h2>
+		<p>Other toys for chinchillas include wheels for running (solid and attached to the cage), PVC pipes, cardboard boxes, ladders, blocks, tubes, and many other endless items. It is important to be aware of the materials that are composed in your chinchilla’s toys, as some can be harmless.  </p>
+	</section>
+</template>
