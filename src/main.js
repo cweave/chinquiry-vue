9fb4805437bec764653de85d14410506be40913f
cweave <cweav3r@gmail.com>
2019-03-10 13:27:35 -0400

    analytics

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+
+const isProd = process.env.NODE_ENV === 'production'
+
+Vue.use(VueAnalytics, {
+	id: 'UA-61317148-5',
+	router,
+	debug: {
+		enabled: !isProd,
+		sendHitTask: isProd
+	}
+})
 
 new Vue({
   el: '#app',
